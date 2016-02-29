f4b68f46243008427f6fa82b592f084d28e721ca
Caroline Lönn <carro.lonn@gmail.com>
2016-02-29 17:57:13 +0100

    Added print to index.html myDinnreView doesn't work

--- js/view/myDinnerView.js
+++ js/view/myDinnerView.js
@@ -1,13 +1,11 @@
 var  MyDinnerView = function (container, model) {
 	model.addObserver(this);
-	console.log("hello");
-	this.numberDinnerOfGuests = container.find("#numberOfGuests");
-	console.log(this.numberOfGuests);
+	this.numberOfDinnerGuests = container.find("#numberOfDinnerGuests");
 	this.backButton2 = container.find("#backButton2");
 	//this.numberOfGuests = container.find(".numberOfGuests2");
 	
 	this.load = function() {
-		this.numberOfGuests.html(model.getNumberOfGuests());
+		this.numberOfDinnerGuests.html(model.getNumberOfGuests());
 	}
 
 	this.update = function(object) {
